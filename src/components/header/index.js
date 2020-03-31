c41c042a2c8d0d1b349cd2038447b52e7fc2b61b
Veranika Barel <veranikabarel@gmail.com>
2020-03-31 18:30:12 +0200

    fix: make navbar responsible

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {Nav} from 'react-bootstrap';
+import {Nav, Navbar} from 'react-bootstrap';
 import {NavLink} from 'react-router-dom';
 import './style.scss';
 
 const Header = () => {
     return (
-            <Nav className='mr-auto justify-content-center'>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/blog'>Blog</NavLink>
-                <NavLink to='/patterns'>Patterns</NavLink>
-                <NavLink to='/contact'>Contact</NavLink>
-            </Nav>
+        <Navbar collapseOnSelect expand="lg">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/blog'>Blog</NavLink>
+                    <NavLink to='/patterns'>Patterns</NavLink>
+                    <NavLink to='/contact'>Contact</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
