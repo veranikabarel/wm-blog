dd49439ae32380dc2d055be06ba5d046ad115875
Veranika Barel <veranikabarel@gmail.com>
2020-04-29 18:58:09 +0200

    fix: ready to deploy

--- src/components/aboutMe/index.js
+++ src/components/aboutMe/index.js
@@ -4,7 +4,7 @@ import './style.scss';
 const AboutMe = () => {
     return (
         <div>
-            <p className='aboutMeText'>Welcome to WOOL MORNING, the place that was created
+            <p className='aboutMeText'>Welcome to the place that was created
                 to inspire, gather like-minded knitters and spread the idea of natural fibers.
                 My name is Veranika and in this blog I will be putting thoughts and feelings
                 into stitches.
