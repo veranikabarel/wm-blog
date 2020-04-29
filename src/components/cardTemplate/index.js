7b327f0da3fad2ff8e28083bccb860cf24006b9c
Veranika Barel <veranikabarel@gmail.com>
2020-04-29 18:48:39 +0200

    feat: add new banner

--- src/components/cardTemplate/index.js
+++ src/components/cardTemplate/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Card, Button} from 'react-bootstrap';
-import postPic from '../../assets/random.png';
+import postPic from '../../assets/sockss.JPG';
 import './style.scss';
 
 const CardTemplate = () => {
